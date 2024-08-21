2bada417508501c4a335754708af35d21e8700cf
lacexxa <daganirgamer@gmail.com>
2024-08-20 23:28:17 -0500

    cambios nocturnos

--- Dolar_PuntoVenta.Presentacion/Frm_Punto_Venta.Designer.cs
+++ Dolar_PuntoVenta.Presentacion/Frm_Punto_Venta.Designer.cs
@@ -32,22 +32,22 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_Punto_Venta));
             this.Tbc_principal = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.Dgv_Listado = new System.Windows.Forms.DataGridView();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txt_Buscar = new System.Windows.Forms.TextBox();
-            this.btn_buscar = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.btn_retornar = new System.Windows.Forms.Button();
-            this.btn_guardar = new System.Windows.Forms.Button();
             this.btn_cancelar = new System.Windows.Forms.Button();
+            this.btn_guardar = new System.Windows.Forms.Button();
+            this.btn_retornar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btn_buscar = new System.Windows.Forms.Button();
+            this.txt_Buscar = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Dgv_Listado = new System.Windows.Forms.DataGridView();
             this.btn_Nuevo = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.btn_Actualizar = new System.Windows.Forms.Button();
             this.btn_Eliminar = new System.Windows.Forms.Button();
             this.btn_Reporte = new System.Windows.Forms.Button();
             this.btn_Salir = new System.Windows.Forms.Button();
-            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.Tbc_principal.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
@@ -67,6 +67,7 @@
             // 
             // tabPage1
             // 
+            this.tabPage1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(78)))), ((int)(((byte)(112)))));
             this.tabPage1.Controls.Add(this.btn_cancelar);
             this.tabPage1.Controls.Add(this.btn_guardar);
             this.tabPage1.Controls.Add(this.btn_retornar);
@@ -78,10 +79,64 @@
             this.tabPage1.Size = new System.Drawing.Size(781, 317);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Mantenimiento";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // btn_cancelar
+            // 
+            this.btn_cancelar.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btn_cancelar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btn_cancelar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(145)))), ((int)(((byte)(134)))));
+            this.btn_cancelar.Location = new System.Drawing.Point(131, 73);
+            this.btn_cancelar.Name = "btn_cancelar";
+            this.btn_cancelar.Size = new System.Drawing.Size(75, 23);
+            this.btn_cancelar.TabIndex = 6;
+            this.btn_cancelar.Text = "Cancelar";
+            this.btn_cancelar.UseVisualStyleBackColor = false;
+            // 
+            // btn_guardar
+            // 
+            this.btn_guardar.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btn_guardar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btn_guardar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(145)))), ((int)(((byte)(134)))));
+            this.btn_guardar.Location = new System.Drawing.Point(212, 73);
+            this.btn_guardar.Name = "btn_guardar";
+            this.btn_guardar.Size = new System.Drawing.Size(75, 23);
+            this.btn_guardar.TabIndex = 5;
+            this.btn_guardar.Text = "Guardar";
+            this.btn_guardar.UseVisualStyleBackColor = false;
+            // 
+            // btn_retornar
+            // 
+            this.btn_retornar.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btn_retornar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btn_retornar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(145)))), ((int)(((byte)(134)))));
+            this.btn_retornar.Location = new System.Drawing.Point(298, 73);
+            this.btn_retornar.Name = "btn_retornar";
+            this.btn_retornar.Size = new System.Drawing.Size(75, 23);
+            this.btn_retornar.TabIndex = 4;
+            this.btn_retornar.Text = "Retornar";
+            this.btn_retornar.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(105, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(312, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(235)))), ((int)(((byte)(211)))));
+            this.label2.Location = new System.Drawing.Point(18, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Punto de Venta";
             // 
             // tabPage2
             // 
+            this.tabPage2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(78)))), ((int)(((byte)(112)))));
             this.tabPage2.Controls.Add(this.btn_buscar);
             this.tabPage2.Controls.Add(this.txt_Buscar);
             this.tabPage2.Controls.Add(this.label1);
@@ -92,24 +147,17 @@
             this.tabPage2.Size = new System.Drawing.Size(781, 317);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Consultas";
-            this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // Dgv_Listado
+            // btn_buscar
             // 
-            this.Dgv_Listado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.Dgv_Listado.Location = new System.Drawing.Point(21, 61);
-            this.Dgv_Listado.Name = "Dgv_Listado";
-            this.Dgv_Listado.Size = new System.Drawing.Size(580, 237);
-            this.Dgv_Listado.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(18, 31);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(40, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Buscar";
+            this.btn_buscar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(145)))), ((int)(((byte)(134)))));
+            this.btn_buscar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(235)))), ((int)(((byte)(211)))));
+            this.btn_buscar.Location = new System.Drawing.Point(283, 28);
+            this.btn_buscar.Name = "btn_buscar";
+            this.btn_buscar.Size = new System.Drawing.Size(75, 23);
+            this.btn_buscar.TabIndex = 3;
+            this.btn_buscar.Text = "Buscar";
+            this.btn_buscar.UseVisualStyleBackColor = false;
             // 
             // txt_Buscar
             // 
@@ -118,57 +166,24 @@
             this.txt_Buscar.Size = new System.Drawing.Size(204, 20);
             this.txt_Buscar.TabIndex = 2;
             // 
-            // btn_buscar
+            // label1
             // 
-            this.btn_buscar.Location = new System.Drawing.Point(283, 28);
-            this.btn_buscar.Name = "btn_buscar";
-            this.btn_buscar.Size = new System.Drawing.Size(75, 23);
-            this.btn_buscar.TabIndex = 3;
-            this.btn_buscar.Text = "Buscar";
-            this.btn_buscar.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(235)))), ((int)(((byte)(211)))));
+            this.label1.Location = new System.Drawing.Point(18, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Buscar";
             // 
-            // label2
+            // Dgv_Listado
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(18, 22);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(81, 13);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Punto de Venta";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(105, 22);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(312, 20);
-            this.textBox1.TabIndex = 1;
-            // 
-            // btn_retornar
-            // 
-            this.btn_retornar.Location = new System.Drawing.Point(298, 58);
-            this.btn_retornar.Name = "btn_retornar";
-            this.btn_retornar.Size = new System.Drawing.Size(75, 23);
-            this.btn_retornar.TabIndex = 4;
-            this.btn_retornar.Text = "Retornar";
-            this.btn_retornar.UseVisualStyleBackColor = true;
-            // 
-            // btn_guardar
-            // 
-            this.btn_guardar.Location = new System.Drawing.Point(212, 58);
-            this.btn_guardar.Name = "btn_guardar";
-            this.btn_guardar.Size = new System.Drawing.Size(75, 23);
-            this.btn_guardar.TabIndex = 5;
-            this.btn_guardar.Text = "Guardar";
-            this.btn_guardar.UseVisualStyleBackColor = true;
-            // 
-            // btn_cancelar
-            // 
-            this.btn_cancelar.Location = new System.Drawing.Point(131, 58);
-            this.btn_cancelar.Name = "btn_cancelar";
-            this.btn_cancelar.Size = new System.Drawing.Size(75, 23);
-            this.btn_cancelar.TabIndex = 6;
-            this.btn_cancelar.Text = "Cancelar";
-            this.btn_cancelar.UseVisualStyleBackColor = true;
+            this.Dgv_Listado.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(235)))), ((int)(((byte)(211)))));
+            this.Dgv_Listado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Dgv_Listado.Location = new System.Drawing.Point(21, 61);
+            this.Dgv_Listado.Name = "Dgv_Listado";
+            this.Dgv_Listado.Size = new System.Drawing.Size(580, 237);
+            this.Dgv_Listado.TabIndex = 0;
             // 
             // btn_Nuevo
             // 
@@ -186,6 +201,16 @@
             this.btn_Nuevo.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btn_Nuevo.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.btn_Nuevo.UseVisualStyleBackColor = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "nuevo.png");
+            this.imageList1.Images.SetKeyName(1, "actualizar.png");
+            this.imageList1.Images.SetKeyName(2, "reporte.png");
+            this.imageList1.Images.SetKeyName(3, "eliminar.png");
+            this.imageList1.Images.SetKeyName(4, "salir.png");
             // 
             // btn_Actualizar
             // 
@@ -255,20 +280,11 @@
             this.btn_Salir.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.btn_Salir.UseVisualStyleBackColor = false;
             // 
-            // imageList1
-            // 
-            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
-            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
-            this.imageList1.Images.SetKeyName(0, "nuevo.png");
-            this.imageList1.Images.SetKeyName(1, "actualizar.png");
-            this.imageList1.Images.SetKeyName(2, "reporte.png");
-            this.imageList1.Images.SetKeyName(3, "eliminar.png");
-            this.imageList1.Images.SetKeyName(4, "salir.png");
-            // 
             // Frm_Punto_Venta
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(145)))), ((int)(((byte)(134)))));
             this.ClientSize = new System.Drawing.Size(969, 521);
             this.Controls.Add(this.btn_Salir);
             this.Controls.Add(this.btn_Reporte);
