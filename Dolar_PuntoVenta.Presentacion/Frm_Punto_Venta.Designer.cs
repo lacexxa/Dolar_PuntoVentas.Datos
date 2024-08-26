32c50a957ab565e71edd1bbb65fd08bc819a54bb
lacexxa <daganirgamer@gmail.com>
2024-08-25 20:34:46 -0500

    commits nocturbos
    como se llamaba ese juego llamado gulo estrain

--- Dolar_PuntoVenta.Presentacion/Frm_Punto_Venta.Designer.cs
+++ Dolar_PuntoVenta.Presentacion/Frm_Punto_Venta.Designer.cs
@@ -29,9 +29,11 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_Punto_Venta));
             this.Tbc_principal = new System.Windows.Forms.TabControl();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.lbl_totalregistros = new System.Windows.Forms.Label();
             this.btn_buscar = new System.Windows.Forms.Button();
             this.txt_Buscar = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -40,7 +42,7 @@
             this.btn_cancelar = new System.Windows.Forms.Button();
             this.btn_guardar = new System.Windows.Forms.Button();
             this.btn_retornar = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Txt_descripcion = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.btn_Nuevo = new System.Windows.Forms.Button();
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
@@ -48,7 +50,6 @@
             this.btn_Eliminar = new System.Windows.Forms.Button();
             this.btn_Reporte = new System.Windows.Forms.Button();
             this.btn_Salir = new System.Windows.Forms.Button();
-            this.lbl_totalregistros = new System.Windows.Forms.Label();
             this.Tbc_principal.SuspendLayout();
             this.tabPage2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.Dgv_Listado)).BeginInit();
@@ -64,7 +65,7 @@
             this.Tbc_principal.Location = new System.Drawing.Point(0, 0);
             this.Tbc_principal.Name = "Tbc_principal";
             this.Tbc_principal.SelectedIndex = 0;
-            this.Tbc_principal.Size = new System.Drawing.Size(969, 355);
+            this.Tbc_principal.Size = new System.Drawing.Size(664, 379);
             this.Tbc_principal.TabIndex = 0;
             // 
             // tabPage2
@@ -78,9 +79,18 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 34);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(961, 317);
+            this.tabPage2.Size = new System.Drawing.Size(656, 341);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Consultas";
+            // 
+            // lbl_totalregistros
+            // 
+            this.lbl_totalregistros.AutoSize = true;
+            this.lbl_totalregistros.Location = new System.Drawing.Point(513, 313);
+            this.lbl_totalregistros.Name = "lbl_totalregistros";
+            this.lbl_totalregistros.Size = new System.Drawing.Size(88, 13);
+            this.lbl_totalregistros.TabIndex = 4;
+            this.lbl_totalregistros.Text = "Total registros : 0";
             // 
             // btn_buscar
             // 
@@ -113,10 +123,26 @@
             // 
             // Dgv_Listado
             // 
+            this.Dgv_Listado.AllowUserToAddRows = false;
+            this.Dgv_Listado.AllowUserToDeleteRows = false;
+            this.Dgv_Listado.AllowUserToOrderColumns = true;
             this.Dgv_Listado.BackgroundColor = System.Drawing.Color.White;
-            this.Dgv_Listado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Dgv_Listado.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.Dgv_Listado.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.Dgv_Listado.ColumnHeadersHeight = 35;
+            this.Dgv_Listado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.Dgv_Listado.EnableHeadersVisualStyles = false;
             this.Dgv_Listado.Location = new System.Drawing.Point(21, 61);
             this.Dgv_Listado.Name = "Dgv_Listado";
+            this.Dgv_Listado.ReadOnly = true;
+            this.Dgv_Listado.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.Dgv_Listado.Size = new System.Drawing.Size(580, 237);
             this.Dgv_Listado.TabIndex = 0;
             // 
@@ -126,12 +152,12 @@
             this.tabPage1.Controls.Add(this.btn_cancelar);
             this.tabPage1.Controls.Add(this.btn_guardar);
             this.tabPage1.Controls.Add(this.btn_retornar);
-            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.Txt_descripcion);
             this.tabPage1.Controls.Add(this.label2);
             this.tabPage1.Location = new System.Drawing.Point(4, 34);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(961, 317);
+            this.tabPage1.Size = new System.Drawing.Size(656, 341);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Mantenimiento";
             // 
@@ -146,6 +172,8 @@
             this.btn_cancelar.TabIndex = 6;
             this.btn_cancelar.Text = "Cancelar";
             this.btn_cancelar.UseVisualStyleBackColor = false;
+            this.btn_cancelar.Visible = false;
+            this.btn_cancelar.Click += new System.EventHandler(this.btn_cancelar_Click);
             // 
             // btn_guardar
             // 
@@ -158,6 +186,7 @@
             this.btn_guardar.TabIndex = 5;
             this.btn_guardar.Text = "Guardar";
             this.btn_guardar.UseVisualStyleBackColor = false;
+            this.btn_guardar.Visible = false;
             // 
             // btn_retornar
             // 
@@ -170,13 +199,16 @@
             this.btn_retornar.TabIndex = 4;
             this.btn_retornar.Text = "Retornar";
             this.btn_retornar.UseVisualStyleBackColor = false;
+            this.btn_retornar.Visible = false;
+            this.btn_retornar.Click += new System.EventHandler(this.btn_retornar_Click);
             // 
-            // textBox1
+            // Txt_descripcion
             // 
-            this.textBox1.Location = new System.Drawing.Point(100, 24);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(312, 20);
-            this.textBox1.TabIndex = 1;
+            this.Txt_descripcion.Location = new System.Drawing.Point(100, 24);
+            this.Txt_descripcion.Name = "Txt_descripcion";
+            this.Txt_descripcion.ReadOnly = true;
+            this.Txt_descripcion.Size = new System.Drawing.Size(312, 20);
+            this.Txt_descripcion.TabIndex = 1;
             // 
             // label2
             // 
@@ -205,6 +237,7 @@
             this.btn_Nuevo.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btn_Nuevo.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.btn_Nuevo.UseVisualStyleBackColor = false;
+            this.btn_Nuevo.Click += new System.EventHandler(this.btn_Nuevo_Click);
             // 
             // imageList1
             // 
@@ -284,21 +317,12 @@
             this.btn_Salir.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.btn_Salir.UseVisualStyleBackColor = false;
             // 
-            // lbl_totalregistros
-            // 
-            this.lbl_totalregistros.AutoSize = true;
-            this.lbl_totalregistros.Location = new System.Drawing.Point(625, 285);
-            this.lbl_totalregistros.Name = "lbl_totalregistros";
-            this.lbl_totalregistros.Size = new System.Drawing.Size(88, 13);
-            this.lbl_totalregistros.TabIndex = 4;
-            this.lbl_totalregistros.Text = "Total registros : 0";
-            // 
             // Frm_Punto_Venta
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(145)))), ((int)(((byte)(134)))));
-            this.ClientSize = new System.Drawing.Size(969, 521);
+            this.ClientSize = new System.Drawing.Size(664, 442);
             this.Controls.Add(this.btn_Salir);
             this.Controls.Add(this.btn_Reporte);
             this.Controls.Add(this.btn_Eliminar);
@@ -333,7 +357,7 @@
         private System.Windows.Forms.Button btn_cancelar;
         private System.Windows.Forms.Button btn_guardar;
         private System.Windows.Forms.Button btn_retornar;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox Txt_descripcion;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btn_Nuevo;
         private System.Windows.Forms.Button btn_Actualizar;
--- Dolar_PuntoVenta.Presentacion/Frm_Punto_Venta.cs
+++ Dolar_PuntoVenta.Presentacion/Frm_Punto_Venta.cs
@@ -30,7 +30,7 @@ namespace Dolar_PuntoVenta.Presentacion
         {
             Dgv_Listado.Columns[0].Width = 100;
             Dgv_Listado.Columns[0].HeaderText = "CODIGO_PV";
-            Dgv_Listado.Columns[1].Width = 250;
+            Dgv_Listado.Columns[1].Width = 435;
             Dgv_Listado.Columns[1].HeaderText = "PUNTO DE VENTA";
         }
 
@@ -54,7 +54,56 @@ namespace Dolar_PuntoVenta.Presentacion
                 MessageBox.Show(ex.Message + ex.StackTrace);
             }
         }
+        private void Estado_BotonesPrincipales(bool lEstado)
+        {
+            btn_Nuevo.Enabled = lEstado;
+            btn_Actualizar.Enabled = lEstado;
+            btn_Eliminar.Enabled = lEstado;          
+            btn_Reporte.Enabled = lEstado;
+            btn_Salir.Enabled = lEstado;
+
+        }
+        private void Limpia_texto()
+        {
+            Txt_descripcion.Text = "";       
+        }
+        private void Estado_texto (bool lEstado)
+        {
+            Txt_descripcion.ReadOnly = !lEstado;
+
+        }
+        private void Estado_BotonesProceso(bool lEstado) {
+
+            btn_cancelar.Visible = lEstado;
+            btn_guardar.Visible = lEstado;
+            btn_retornar.Visible = !lEstado;
+
+        }
 
         #endregion
+
+        private void btn_Nuevo_Click(object sender, EventArgs e)
+        {
+            this.Estado_BotonesPrincipales(false);
+            this.Estado_BotonesProceso(true);
+            this.Limpia_texto();
+            this.Estado_texto(true);
+            Tbc_principal.SelectedIndex = 1;
+            Txt_descripcion.Focus();
+        }
+
+        private void btn_cancelar_Click(object sender, EventArgs e)
+        {
+            this.Limpia_texto();
+            this.Estado_texto(false);
+            this.Estado_BotonesPrincipales(true);
+            this.Estado_BotonesProceso(false);
+            Tbc_principal.SelectedIndex = 0;
+        }
+
+        private void btn_retornar_Click(object sender, EventArgs e)
+        {
+            Tbc_principal.SelectedIndex = 0;
+        }
     }
 }
